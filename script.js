b327c413387de240f082a8b69dbefb50036f0433
zane-dot <22258396@life.hkbu.edu.hk>
2025-04-14 20:00:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 window.onload = function() {
-  document.querySelector('form').addEventListener('submit', function (e) {
-      e.preventDefault();
-      const inputText = document.getElementById('inputText').value;
-      const resultDiv = document.getElementById('result');
-      const regex = /<script>ALERT\([^)]*\)/i;
-      if (regex.test(inputText)) {
-          resultDiv.textContent = 'flag_HPFporkonsale';
-      } else {
-          resultDiv.textContent = 'Keep thinking!This question is about web security.';
-      }
-  });
-};
+    document.querySelector('form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        const inputText = document.getElementById('inputText').value;
+        const resultDiv = document.getElementById('result');
+        const regex = /<script>ALERT\([^)]*\)/i;
+        if (regex.test(inputText)) {
+            // Obfuscated flag
+            const encodedFlag = 'ZmxhZ19IUEZwb3Jrb25zYWxl';
+            resultDiv.textContent = atob(encodedFlag);
+        } else {
+            resultDiv.textContent = 'Keep thinking!This question is about web security.';
+        }
+    });
+  };
